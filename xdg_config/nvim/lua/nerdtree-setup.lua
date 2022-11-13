52101689daaa817f3396cf818ef0b9769c9d1a99
Kei Takahashi <ktakahashi.dev@gmail.com>
2022-11-13 11:25:26 -0500

    nerdtree setup

---
+++ xdg_config/nvim/lua/nerdtree-setup.lua
@@ -0,0 +1,7 @@
+
+vim.keymap.set('n', '<C-n>', '<cmd>NERDTreeToggle<cr>')
+vim.keymap.set('n', '<leader>gf', '<cmd>NERDTreeFind<cr>')
+
+vim.g.NERDTreeWinSize = 60
+vim.g.NERDTreeMinimalUI = 1
+vim.g.NERDTreeShowLineNumbers = 1
