0fd9b6fd4431d74c251356ed910f35bef2e06733
Kei Takahashi <ktakahashi.dev@gmail.com>
2022-12-05 22:37:50 -0500

    easy wat to go between .cpp and .h

---
+++ xdg_config/nvim/lua/my-util.lua
@@ -0,0 +1,46 @@
+local map = {
+  cpp = { "h" },
+  cxx = { "h", "hxx" },
+  h =  { "cpp", "cxx" },
+}
+
+local function try_open_pairing_file()
+  -- get current file name
+  local path = vim.api.nvim_buf_get_name(0)
+
+  -- change it to .h or .cpp
+  --   there should be a better way than doing this manually like this?
+  local pos = 1
+  while true do
+    local new_pos = string.find(path, "%.", pos)
+    if new_pos ~= nil then
+      pos = new_pos + 1
+    else
+      break
+    end
+  end
+
+  local ext = string.sub(path, pos)
+  local alt_exts = map[ext]
+
+  local found = false
+
+  if alt_exts ~= nil then
+    for _, alt_ext in ipairs(alt_exts) do
+      local alt_file = string.sub(path, 1, pos - 1) .. alt_ext
+      -- check if that file exists
+      if vim.fn.filereadable(alt_file) == 1 then
+        vim.cmd({ cmd = 'edit', args = { alt_file } })
+        found = true
+        break
+      end
+    end
+  end
+
+  if not found then
+    print("no pairing file found")
+  end
+end
+
+-- o[pen] a[lternate]
+vim.keymap.set('n', '<leader>oa', try_open_pairing_file)
