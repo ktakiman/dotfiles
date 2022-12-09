df54e97584c79631f5654cc9d5463b52cd86f185
Kei Takahashi <ktakahashi.dev@gmail.com>
2022-12-08 20:54:32 -0500

    toggle background transparency

--- xdg_config/nvim/lua/color-setup.lua
+++ xdg_config/nvim/lua/color-setup.lua
@@ -1,3 +1,14 @@
--- if packer_loaded('gruvbox') then
-  vim.cmd({cmd='colo',args={'gruvbox'}})
--- end
+vim.cmd({ cmd='colo', args={'gruvbox'} })
+
+vim.cmd({ cmd='hi', args={'Comment', 'cterm=italic'} })
+
+local org_bg = '235' -- how can I capture this on the fly
+local cur_bg = org_bg
+
+local function ToggleBg()
+  local to_apply = cur_bg == org_bg and 'none' or org_bg
+  cur_bg = to_apply
+  vim.cmd({ cmd='hi', args={'Normal', 'ctermbg=' .. to_apply} })
+end
+
+vim.keymap.set('n', '<leader>xx', ToggleBg)
