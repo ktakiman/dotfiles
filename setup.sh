6837c51d1995644038a3d1812d1e3a58f0989a1e
Kei Takahashi <ktakahashi.dev@gmail.com>
2022-12-16 23:20:30 -0500

    fix bad fix

--- setup.sh
+++ setup.sh
@@ -41,8 +41,8 @@ ln -s "${DOTFILEDIR}/xdg_config/nvim/init.lua" ~/.config/nvim/
 ln -s "${DOTFILEDIR}/xdg_config/nvim/lua" ~/.config/nvim/
 
 # setup packer.nvim
+PACKERDIR=~/.local/share/nvim/site/pack/packer/start/packer.nvim
 if [ -d "$PACKERDIR" ];then
-  PACKERDIR=~/.local/share/nvim/site/pack/packer/start/packer.nvim
   mkdir -p "$PACKERDIR"
   git clone --depth 1 https://github.com/wbthomason/packer.nvim $PACKERDIR
 fi
