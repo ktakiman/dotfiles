b26a278083964ddf0309439db3c9022d1e57c78c
Kei Takahashi <ktakahashi.dev@gmail.com>
2023-01-01 09:46:19 -0500

    update floaterm setup

--- xdg_config/nvim/lua/floaterm-setup.lua
+++ xdg_config/nvim/lua/floaterm-setup.lua
@@ -1,7 +1,7 @@
 -- vim-floaterm is vim plugin
--- is there better nvim-native alternative
-vim.g.floaterm_borderchars = '        '
-vim.g.floaterm_width = 0.99
-vim.g.floaterm_height = 0.99
+-- is there better nvim-native alternative ?
+-- vim.g.floaterm_borderchars = '        '
+vim.g.floaterm_width = 0.95
+vim.g.floaterm_height = 0.95
 
-vim.cmd({ cmd = 'hi', args = { 'link', 'FloatermBorder', 'NonText' } })
+-- vim.cmd({ cmd = 'hi', args = { 'link', 'FloatermBorder', 'NonText' } })
