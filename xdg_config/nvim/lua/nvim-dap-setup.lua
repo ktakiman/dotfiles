e17fbad582d87a4f910f4e0e6d1967586a865a06
Kei Takahashi <ktakahashi.dev@gmail.com>
2023-01-08 01:41:34 -0500

    dap start debug session immediately

--- xdg_config/nvim/lua/nvim-dap-setup.lua
+++ xdg_config/nvim/lua/nvim-dap-setup.lua
@@ -25,97 +25,120 @@ local function getCache(lang, key, callback)
 
   -- get the new value, callback can just return the cached value, overwrite with new value
   -- or get the value if not cached yet
-  local newValue = callback(cached, cached ~= nil)
+  local newValue = callback(cached)
   cache[lang][cwd][key] = newValue
 
   return newValue
 end
 
-local function getProgram(lang)
-  -- P('program')
-  local v = getCache(lang, 'program', function(cached_value, was_cached)
-    local value_to_show = was_cached and cached_value or vim.fn.getcwd() .. '/'
-    return vim.fn.input('Path to executable: ', value_to_show, 'file')
-  end)
+local function prompt(skip, cur_value, def_value, msg, kind)
+  if skip and cur_value then
+    return cur_value
+  end
 
-  print('program = ' .. v)
-
-  return v
+  local value_to_show = cur_value and cur_value or def_value
+  if kind then
+    return vim.fn.input(msg, value_to_show, kind)
+  else
+    return vim.fn.input(msg, value_to_show) -- pass 'nil' for 'kind' results in an error
+  end
 end
 
-local function getPid(lang)
-  local v = getCache(lang, 'pid', function(cached_value, was_cached)
-    local value_to_show = was_cached and cached_value or ''
-    return tonumber(vim.fn.input('PID: ', value_to_show))
+local function getProgram(lang, skip_prompt)
+  return getCache(lang, 'program', function(cached_value)
+    return prompt(skip_prompt, cached_value, vim.fn.getcwd() .. '/', 'Path to executable: ', 'file')
   end)
-
-  print('pid = ' .. v)
-
-  return v
 end
 
-local function getArgs(lang)
-  local v = getCache(lang, 'args', function(cached_value, was_cached)
-    local value_to_show = was_cached and cached_value or ''
-    return vim.fn.input('args: ', value_to_show)
+local function getPid(lang, skip_prompt)
+  return getCache(lang, 'pid', function(cached_value)
+    return tonumber(prompt(skip_prompt, cached_value, '', 'PID: '))
   end)
+end
 
-  print('args = ' .. v)
-  return v == '' and nil or { v }
+local function getArgs(lang, skip_prompt)
+  return getCache(lang, 'args', function(cached_value)
+    return prompt(skip_prompt, cached_value, '', 'Args: ')
+  end)
 end
 
 dap.clearCache = function()
-  cache.cpp = {}
+  cache = {}
 end
 
 dap.printCache = function()
   P(cache)
 end
 
+local g_skip_prompt = false
+
+local launch = {
+  name = 'Launch',
+  type = 'lldb',
+  request = 'launch',
+  program = function()
+    print('g_skip_prompt', g_skip_prompt)
+    return getProgram('cpp', g_skip_prompt)
+  end,
+  cwd = '${workspaceFolder}',
+  stopOnEntry = false,
+  args = function()
+    return getArgs('cpp', g_skip_prompt)
+  end,
+  --
+  -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+  --
+  --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+  --
+  -- Otherwise you might get the following error:
+  --
+  --    Error on launch: Failed to attach to the target process
+  --
+  -- But you should be aware of the implications:
+  -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+  -- runInTerminal = false,
+}
+
+local attach = {
+  name = 'Attach',
+  type = 'lldb',
+  request = 'attach',
+  program = function()
+    return getProgram('cpp')
+  end,
+  pid = function()
+    return getPid('cpp', g_skip_prompt)
+  end,
+  cwd = '${workspaceFolder}',
+  stopOnEntry = false,
+  args = {},
+}
+
 dap.configurations.cpp = {
-  {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-    program = function()
-      return getProgram('cpp')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = function()
-      return getArgs('cpp')
-    end,
-    --
-    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-    --
-    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --
-    -- Otherwise you might get the following error:
-    --
-    --    Error on launch: Failed to attach to the target process
-    --
-    -- But you should be aware of the implications:
-    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-    -- runInTerminal = false,
-  },
-  {
-    name = 'Attach',
-    type = 'lldb',
-    request = 'attach',
-    program = function()
-      return getProgram('cpp')
-    end,
-    pid = function()
-      return getPid('cpp')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-  },
+  launch,
+  attach,
 }
 
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
+
+-- the goal here is start debug session as last run without prompting for input
+local function StartNoPrompt()
+  print(dap.session())
+  if dap.session() then
+    print('dap.continue()')
+    dap.continue()
+  else
+    print('dap.run()')
+    g_skip_prompt = true
+    dap.run(launch)
+  end
+end
+
+local function StartWithPrompt()
+  g_skip_prompt = false
+  dap.continue()
+end
 
 -- my custom functions
 local widgets = require('dap.ui.widgets')
@@ -123,7 +146,8 @@ dap.toggleScopes = function()
   widgets.centered_float(widgets.scopes).toggle()
 end
 
-vim.keymap.set('n', '<f5>', dap.continue)
+vim.keymap.set('n', '<f5>', StartNoPrompt)
+vim.keymap.set('n', '<f6>', StartWithPrompt)
 vim.keymap.set('n', '<f10>', dap.step_over)
 vim.keymap.set('n', '<f11>', dap.step_into)
 vim.keymap.set('n', '<s-f11>', dap.step_out)
